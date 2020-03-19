41867e9809165aeba468c0d9788645bee92f044b
MrFlou <Simonflou@gmail.com>
2020-03-19 12:06:34 +0100

    new flight

--- Assets/SimpleFlyController.cs
+++ Assets/SimpleFlyController.cs
@@ -2,13 +2,11 @@
 
     [RequireComponent(typeof(Rigidbody))]
     public class SimpleFlyController : MonoBehaviour
-    {
-
-        
+    {        
         public float thrust = 10f;
         public Vector3 turnTorque = new Vector3(90f, 25f, 45f);
         public float forceMult = 1000f;
-        
+        public float downThrust = 0;
         private float pitch = 0f;
         private float yaw = 0f;
         private float roll = 0f;
@@ -17,11 +15,11 @@
         public float Yaw { set { yaw = Mathf.Clamp(value, -1f, 1f); } get { return yaw; } }
         public float Roll { set { roll = Mathf.Clamp(value, -1f, 1f); } get { return roll; } }
 
-        private Rigidbody xrigid;
+        private Rigidbody rigid;
 
         private void Awake()
         {
-            xrigid = GetComponent<Rigidbody>();
+           rigid = GetComponent<Rigidbody>();
         }
 
         private void Update() {
@@ -37,8 +35,17 @@
         {
             press = true;
         }
-        thrust -= 10f * Time.deltaTime;
-        thrust = Mathf.Clamp(thrust, 0f, 50f);
+        if (thrust > 10)
+        {
+            thrust -= 15f * Time.deltaTime;
+        } else { thrust -= 15f * Time.deltaTime; }
+        
+        thrust = Mathf.Clamp(thrust, 2f, 50f);
+        downThrust = thrust.Remap(2f, 50f, 20f, 0f);
+        if(transform.localPosition.y <= 2.5f)
+        {
+            downThrust = 0f;
+        }
         roll = Input.GetAxis("Horizontal");
 
         pitch = Input.GetAxis("Vertical");
@@ -47,7 +54,22 @@
 
         private void FixedUpdate()
         {
-            xrigid.AddRelativeForce(Vector3.forward * thrust * forceMult, ForceMode.Force);
-            xrigid.AddRelativeTorque(new Vector3(turnTorque.x * pitch, turnTorque.y * yaw, -turnTorque.z * roll) * forceMult, ForceMode.Force);
+        rigid.AddRelativeForce(Vector3.forward * thrust * forceMult, ForceMode.Force);
+        rigid.AddForce(Vector3.down * downThrust * forceMult, ForceMode.Force);
+        rigid.AddRelativeTorque(new Vector3(turnTorque.x * pitch, turnTorque.y * yaw, -turnTorque.z * roll) * forceMult, ForceMode.Force);
+        if (rigid.velocity.magnitude > 150)
+        {
+            rigid.velocity = rigid.velocity.normalized * 150;
+        }
+
         }
     }
+
+public static class ExtensionMethods{
+
+    public static float Remap(this float value, float from1, float to1, float from2, float to2)
+    {
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+    }
+
+}
