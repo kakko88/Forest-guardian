6f382adbf1bf8b75c5ae2291fcbc4f422012e478
esandk17 <esandk17@student.aau.dk>
2020-03-24 11:08:53 +0100

    Menu
    Menu and some shop stuff

---
+++ Assets/UI_SHOP.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class UI_SHOP : MonoBehaviour
+{
+    private Transform container;
+    private Transform shopItemTemplate;
+
+    private void Awake()
+    {
+        container = transform.Find("container");
+        shopItemTemplate = container.Find("shopItemTemplate");
+        shopItemTemplate.gameObject.SetActive(false);
+    }
+
+    private void Start()
+    {
+        CreateUpgradeButton("Speed Upgrade", Upgrades.GetCost(Upgrades.ItemType.SpeedUpgrade), 0);
+        CreateUpgradeButton("Beak Upgrade", Upgrades.GetCost(Upgrades.ItemType.BeakUpgrade), 1);
+        CreateUpgradeButton("Pellet Gun Upgrade", Upgrades.GetCost(Upgrades.ItemType.PelletGun), 2);
+    }
+    private void CreateUpgradeButton(string itemName, int itemCost, int positionIndex)
+    {
+        Transform shopItemTransform = Instantiate(shopItemTemplate, container);
+        shopItemTransform.gameObject.SetActive(true);
+        RectTransform shopItemRectTransform = shopItemTransform.GetComponent<RectTransform>();
+
+        float shopItemHeight = 80f;
+        shopItemRectTransform.anchoredPosition = new Vector2(125, 150-shopItemHeight * positionIndex);
+
+        shopItemTransform.Find("textName").GetComponent<TextMeshProUGUI>().SetText(itemName);
+        shopItemTransform.Find("textCost").GetComponent<TextMeshProUGUI>().SetText(itemCost.ToString());
+
+
+    
+    }
+}
---
+++ Assets/Upgrades.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Upgrades
+{
+    public enum ItemType
+    {
+        SpeedUpgrade,
+        BeakUpgrade,
+        PelletGun,
+
+
+    }
+    public static int GetCost(ItemType itemType)
+    {
+        switch (itemType)
+        {
+            default:
+            case ItemType.SpeedUpgrade: return 50;
+            case ItemType.BeakUpgrade: return 30;
+            case ItemType.PelletGun: return 100;
+        }
+    }
+}
