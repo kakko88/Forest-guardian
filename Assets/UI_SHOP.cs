29245dd18cbb33341f6228a1dde0663f521d5185
esandk17 <esandk17@student.aau.dk>
2020-03-31 12:00:40 +0200

    shop ready for merge

--- Assets/UI_SHOP.cs
+++ Assets/UI_SHOP.cs
@@ -9,6 +9,7 @@ public class UI_SHOP : MonoBehaviour
 {
     private Transform container;
     private Transform shopItemTemplate;
+    private IShopCustomer shopCustomer;
 
     private void Awake()
     {
@@ -19,22 +20,31 @@ public class UI_SHOP : MonoBehaviour
 
     private void Start()
     {
-        CreateUpgradeButton("Speed Upgrade", Upgrades.GetCost(Upgrades.ItemType.SpeedUpgrade), 0);
-        CreateUpgradeButton("Beak Upgrade", Upgrades.GetCost(Upgrades.ItemType.BeakUpgrade), 1);
-        CreateUpgradeButton("Pellet Gun Upgrade", Upgrades.GetCost(Upgrades.ItemType.PelletGun), 2);
+        CreateUpgradeButton(Upgrades.ItemType.SpeedUpgrade, "Speed Upgrade", Upgrades.GetCost(Upgrades.ItemType.SpeedUpgrade), 0);
+        CreateUpgradeButton(Upgrades.ItemType.BeakUpgrade, "Beak Upgrade", Upgrades.GetCost(Upgrades.ItemType.BeakUpgrade), 1);
+        CreateUpgradeButton(Upgrades.ItemType.PelletGun, "Pellet Gun Upgrade", Upgrades.GetCost(Upgrades.ItemType.PelletGun), 2);
     }
-    private void CreateUpgradeButton(string itemName, int itemCost, int positionIndex)
+    private void CreateUpgradeButton(Upgrades.ItemType itemType, string itemName, int itemCost, int positionIndex)
     {
         Transform shopItemTransform = Instantiate(shopItemTemplate, container);
         shopItemTransform.gameObject.SetActive(true);
         RectTransform shopItemRectTransform = shopItemTransform.GetComponent<RectTransform>();
 
         float shopItemHeight = 80f;
-        shopItemRectTransform.anchoredPosition = new Vector2(125, 150-shopItemHeight * positionIndex);
+        shopItemRectTransform.anchoredPosition = new Vector2(125, 250-shopItemHeight * positionIndex);
 
         shopItemTransform.Find("textName").GetComponent<TextMeshProUGUI>().SetText(itemName);
         shopItemTransform.Find("textCost").GetComponent<TextMeshProUGUI>().SetText(itemCost.ToString());
 
+
+        Button btn = shopItemRectTransform.GetComponent<Button>();
+        btn.onClick.AddListener(() => TryBuyItem(itemType));
+
+        void TryBuyItem(Upgrades.ItemType newitemType)
+        {
+            shopCustomer.BoughtItem(itemType);
+            Debug.Log("Clicked Button");
+        }
 
     
     }
