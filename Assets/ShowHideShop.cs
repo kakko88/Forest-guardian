4f5ffa173799f845c2e79d36e142280900c1c295
MrFlou <Simonflou@gmail.com>
2020-04-20 12:14:56 +0200

    shit fucked

--- Assets/ShowHideShop.cs
+++ Assets/ShowHideShop.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ShowHideShop : MonoBehaviour
 {
     public static bool showShop;
+    public GameObject aim;
     void Start()
     {
         
@@ -16,10 +17,13 @@ public class ShowHideShop : MonoBehaviour
             if (showShop == false)
             {
                 child.gameObject.SetActive(false);
+                aim.gameObject.SetActive(true);
             }
             else if (showShop == true)
             {
+                aim.gameObject.SetActive(false);
                 child.gameObject.SetActive(true);
+                
             }
         }
 
